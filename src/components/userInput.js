2ba0cb8b67aa9973814b60650668ce8286e6be47
Jordane-Allen <jordane.allen7@gmail.com>
2021-01-18 22:32:38 -0500

    Formatted Form

--- src/components/userInput.js
+++ src/components/userInput.js
@@ -1,52 +1,72 @@
-import validateInput from "./ValidateInput"
-import React from "react";
+import validateInput from './ValidateInput';
+import React from 'react';
 import TextField from '@material-ui/core/TextField';
-// import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
+class UserWords extends React.Component {
+	constructor(props) {
+		super(props);
 
-class UserWords extends React.Component{
-    constructor(props){
-    super(props);
-    
-    this.state ={
-        user:{ 
-            value: '',
-            error: '',},
-        result:{
-            value:'',
-        },
-        
-        }
-        // handleChange = event =>{
-        //     this.setState(
-        //         {[event.target.]}
-        //     )
-        //     const InputError = validateInput(this.state.user.)}
-    }
-    render(){
-        let style = {
-            color:"#3678FA",
-            fontSize:"30px",
-            fontWeight:"bold"
-        }
-        return (
-        <div>
-            <div>
-                <p style = {style}>Word Translater</p>
-            </div>
-            <div>
-            {/* <Grid> */}
-            <form noValidate autoComplete="off">
-                <TextField id="outlined-multiline-static" label="Multiline" multiline rows={4} defaultValue="Default Value" variant="outlined"></TextField>
-            </form>
-            {/* </Grid> */}
-            </div>
-        </div>
-            
-        )
-    }
+		this.state = {
+			user: {
+				value: '',
+				error: '',
+			},
+			result: {
+				value: '',
+			},
+		};
+		// handleChange = event =>{
+		//     this.setState(
+		//         {[event.target.]}
+		//     )
+		//     const InputError = validateInput(this.state.user.)}
+	}
+	render() {
+		let style = {
+			color: '#3678FA',
+			fontSize: '30px',
+			fontWeight: 'bold',
+		};
 
+		return (
+			<div>
+				<div>
+					<p style={style}>Word Translater</p>
+				</div>
+				<div>
+					<Grid md="4">
+						<form noValidate autoComplete="off">
+							<Grid>
+								<TextField
+									fullWidth
+									id="outlined-multiline-static"
+									label="ENTER TEXT"
+									multiline
+									rows={4}
+									placeholder="Please Enter The Text You Would Like To Translate"
+									variant="outlined"
+								></TextField>
+							</Grid>
+							<br />
+							<Grid>
+								<TextField
+									fullWidth
+									id="outlined-multiline-static"
+									label="TRANSLATION"
+									multiline
+									rows={4}
+									placeholder="TRANSLATION"
+									variant="outlined"
+								></TextField>
+							</Grid>
+							<br />
+						</form>
+					</Grid>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default UserWords
+export default UserWords;
