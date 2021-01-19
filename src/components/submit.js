4609f1bec9189a5cfed3ea4ead8ff9c265348a97
Jordane-Allen <jordane.allen7@gmail.com>
2021-01-18 22:36:02 -0500

    Changed Button name to Translate

--- src/components/submit.js
+++ src/components/submit.js
@@ -1,8 +1,11 @@
 import Button from '@material-ui/core/Button';
-   function Submit () {
-       
-       return(<div><Button variant="contained" color="primary">
-       Primary
-     </Button></div>)}
-export {Submit}
-   
+function Submit() {
+	return (
+		<div>
+			<Button variant="contained" color="primary">
+				Translate
+			</Button>
+		</div>
+	);
+}
+export default Submit;
